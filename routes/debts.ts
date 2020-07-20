a538f9ac827b0a25f4f8e8dedc96fbb78f8af8c7
teepiik <teemu.piik@gmail.com>
2020-07-20 15:28:31 +0300

    heroku deploy

--- routes/debts.ts
+++ routes/debts.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+//import dotenv from 'dotenv'
 import debtService from '../services/debtService'
 
 /* eslint @typescript-eslint/no-unsafe-assignment: 0 */
 /* eslint @typescript-eslint/restrict-plus-operands: 0 */
 
 const router = express.Router()
+//const password = process.env.PASSWORD ?? ''
 
 router.get('/', async (_req, res) => {
   const debts = await debtService.getAllDebts()
@@ -12,6 +14,7 @@ router.get('/', async (_req, res) => {
 })
 
 router.post('/', async (req, res) => {
+  // add password check here req.body.password
   try {
     const {
       date_origin,
